e261b55bd81b5507feb1285742e82dbfe7882461
INADA Naoki <inada-n@klab.com>
2014-03-03 17:04:44 +0900

    Add classifiers

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(name="idlite",
       version="0.0.1",
       packages=find_packages(),
       scripts=['bin/idlite'],
-      install_requires=['PLY'])
+      install_requires=['PLY'],
+      classifiers=[
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 2.7"],
+      )
