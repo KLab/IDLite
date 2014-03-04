04c23094002c4ee6a7cde89ef472433353e6d1bc
INADA Naoki <inada-n@klab.com>
2014-03-05 02:13:29 +0900

    0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="idlite",
-      version="0.0.1",
+      version="0.0.2",
       packages=find_packages(),
       scripts=['bin/idlite'],
       install_requires=['PLY'],
