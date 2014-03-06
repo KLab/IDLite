d87502286b1ed16903da2575050f504d56803781
INADA Naoki <inada-n@klab.com>
2014-03-06 14:33:56 +0900

    Add long_description.

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    long_description = f.read()
 
 setup(name="idlite",
       version="0.0.2",
       packages=find_packages(),
       scripts=['bin/idlite'],
       install_requires=['PLY'],
+      long_description=long_description,
       classifiers=[
           "Programming Language :: Python :: 3.3",
-          "Programming Language :: Python :: 2.7"],
+          "Programming Language :: Python :: 2.7",
+      ],
       )
