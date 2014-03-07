59c7699de0a5772a86aaa05e3040de465011a4bd
INADA Naoki <inada-n@klab.com>
2014-03-07 22:06:05 +0900

    Warn about undefined class and enums.

--- idlite/main.py
+++ idlite/main.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 import argparse
+from functools import partial
 import sys
 
 from idlite.parser import parser
@@ -13,6 +14,40 @@ def build_argparser():
     parser.add_argument('files', nargs='+')
     return parser
 
+
+def check_undefined(spec):
+    premitives = ("object", "float", "double", "int", "long", "bool", "string")
+    warn = partial(print, file=sys.stderr)
+    classes = {}
+    enums = {}
+    for typedef in spec:
+        if isinstance(typedef, types.Enum):
+            enums[typedef.name] = typedef
+        else:
+            assert isinstance(typedef, types.Class)
+            classes[typedef.name] = typedef
+
+    for cls in classes.values():
+        for field in cls.fields:
+            fieldtype = field.type
+            if isinstance(fieldtype, types.List):
+                fieldtype = fieldtype.T
+
+            if isinstance(fieldtype, str):
+                if fieldtype.endswith('?'):
+                    fieldtype = fieldtype[:-1]
+
+                if field.enum:
+                    if fieldtype not in enums:
+                        warn(fieldtype, "enum is not defined")
+                        enums[fieldtype] = None  # Don't warn twice.
+                elif fieldtype not in premitives:
+                    if fieldtype not in classes:
+                        warn(fieldtype, "class is not defined")
+                        classes[fieldtype] = None
+
+
+
 def main():
     argparser = build_argparser()
     args = argparser.parse_args()
@@ -25,6 +60,8 @@ def main():
 
     type_order = {types.Enum: 0, types.Class: 1}
     spec.sort(key=lambda s: (type_order[type(s)], s.name))
+
+    check_undefined(spec)
 
     if args.output_type == 'unity':
         unitygen.generate(spec, sys.stdout, args.namespace)
